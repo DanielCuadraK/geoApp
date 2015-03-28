b76d527b5cddfe802287de10e60dbf0d8433f7f6
dck26 <daniel.cuadra@gmail.com>
2015-03-28 12:50:34 -0600

    1

---
+++ js/actions.js
@@ -0,0 +1,45 @@
+var fn = {
+    ready: function(){
+            document.addEventListener("deviceready",fn.init,false);
+    },
+    init: function(){
+        navigator.geolocation.getCurrentPosition(fn.mapa, fn.error);
+    },
+    mapa: function(p){
+        //Posición del mapa
+				var latlng = new google.maps.LatLng(p.coords.latitude, p.coords.longitude);
+				var myOptions = {
+					zoom: 8,
+					center: latlng,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+				var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+				//Marcador
+				var marker = new google.maps.Marker({
+					position: latlng, 
+					map: map, 
+					title:"Mi posición"
+				});
+    },
+    error: function(err){
+        alert(err.code);
+    }
+};
+
+function initialize() {
+				//Posición del mapa
+				var latlng = new google.maps.LatLng(-34.397, 150.644);
+				var myOptions = {
+					zoom: 8,
+					center: latlng,
+					mapTypeId: google.maps.MapTypeId.ROADMAP
+				};
+				var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
+				//Marcador
+				var marker = new google.maps.Marker({
+					position: latlng, 
+					map: map, 
+					title:"Mi posición"
+				});
+			}
+$(fn.ready);
