6b7582806693818e82370ecec89db87b75d661d8
dck26 <daniel.cuadra@gmail.com>
2015-03-28 12:58:03 -0600

    1

--- js/actions.js
+++ js/actions.js
@@ -25,21 +25,4 @@ var fn = {
         alert(err.code);
     }
 };
-
-function initialize() {
-				//Posición del mapa
-				var latlng = new google.maps.LatLng(-34.397, 150.644);
-				var myOptions = {
-					zoom: 8,
-					center: latlng,
-					mapTypeId: google.maps.MapTypeId.ROADMAP
-				};
-				var map = new google.maps.Map(document.getElementById("map_canvas"),myOptions);
-				//Marcador
-				var marker = new google.maps.Marker({
-					position: latlng, 
-					map: map, 
-					title:"Mi posición"
-				});
-			}
 $(fn.ready);
